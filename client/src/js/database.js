0c58325a7960a3846f3edeb2cc2dec22c35ce7f5
Samuel Mandel <slman.760@gmail.com>
2022-05-25 22:27:40 -0700

    get all content from db added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,14 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
 console.log('Added content to db')
-const jateDb = await openDB('jate', 1);
+const jateDb = await openDB('jate', 1)
 const tx = jateDb.transaction('jate', 'readwrite')
 const store = tx.objectStore('jate')
 const request = store.add({content: content})
+}
 // TODO: Add logic for a method that gets all the content from the database
 
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+console.log('Get from db')
+const jateDb = await openDB('jate', 1)
+const tx = jateDb.transaction('jate', 'readonly')
+const store = tx.objectStore('jate')
+const request = store.getAll();
+const result = await request;
+console.log('confirming result.value', result)
+return
+}
 
 initdb();
